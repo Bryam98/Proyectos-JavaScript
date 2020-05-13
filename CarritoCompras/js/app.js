8caa17f8adf6908d0e2b4a6ac2534bf6457a3dd0
Alejandra Camacho <camacho.ale94@gmail.com>
2020-05-12 22:31:12 -0500

    inserciones a carrito

--- CarritoCompras/js/app.js
+++ CarritoCompras/js/app.js
@@ -1,6 +1,7 @@
 //variables
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
+const listaCursos = document.querySelector('#lista-carrito tbody');
 
 //listeners
 cargarEventListener();
@@ -25,5 +26,29 @@ function comprarCurso(e) {
 
 //lee los datos del curso
 function leerDatosCurso(curso) {
-    console.log(curso);
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
+    }
+
+    insertarCarrito(infoCurso);
+}
+
+//muestra el curso seleccionado en el carrito
+function insertarCarrito(curso) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${curso.imagen}" width="100px">
+        </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+        </td>
+    `;
+
+    listaCursos.appendChild(row);
 }
